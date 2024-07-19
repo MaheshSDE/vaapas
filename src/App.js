b70953a006299c24c12a024efaf45a89078f5458
mahesh <pmahesh493605@gmail.com>
2024-07-19 09:16:35 +0000

    Vaapas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 
 import Header from './components/Header'
 import Home from './components/Home'
 import Movies from './components/Movies'
-// import NotFound from './components/NotFound'
+import NotFound from './components/NotFound'
 
 import './App.css'
 
@@ -13,8 +13,8 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/movies" component={Movies} />
-      {/* <Route exact path="/not-found" component={NotFound} />
-      <Redirect to="/not-found" /> */}
+      <Route exact path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
     </Switch>
   </BrowserRouter>
 )
